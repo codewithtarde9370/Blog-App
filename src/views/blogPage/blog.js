b2f2362ab9a30fc8f0184f348ac4430fa7374296
codewithtarde9370 <teja36491@gmail.com>
2024-06-07 17:01:12 +0530

    readme added

--- src/views/blogPage/blog.js
+++ src/views/blogPage/blog.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import BlogData from '../../data';
 import './blog.css';
+import Navbar from '../../components/navbar';
 
 function Blog() {
     const { id } = useParams();
     const selectedBlog = BlogData.find((blog) => blog.id === id);
 
     return (
-        <div className="selected-blog ">
+      <>
+      <Navbar/>
+      
+      <div className="selected-blog ">
             <h1>{selectedBlog.title}</h1>
             <div className='author-data'>
                 <span className='author-name'>{selectedBlog.author.name}</span>|
@@ -17,7 +21,7 @@ function Blog() {
               <img src={selectedBlog.blogImg} alt='blog' className='blog-image'></img>
             <p className='content'>{selectedBlog.about}</p>
             
-        </div>
+        </div></>
     );
 }
 
